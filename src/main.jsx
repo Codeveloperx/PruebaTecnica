237f309c560c7b5c225005b225d1898102dc17f9
Camilo Orozco HDZ <camilo.orozcohdz@gmail.com>
2022-11-08 20:11:46 -0500

    Componenete de la tabla responive para mostrar los datos

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Navbar from './components/Navbar'
+import Table from './components/Table'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello Wordl</h1>
+    <Navbar/>
+    <Table/>
   </React.StrictMode>
 )
