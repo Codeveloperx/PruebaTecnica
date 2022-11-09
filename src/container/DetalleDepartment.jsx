44f387134168edad38a37c5eee4784281985589e
Camilo Orozco HDZ <camilo.orozcohdz@gmail.com>
2022-11-08 20:17:59 -0500

    creando componente para el detalle del departamento seleccionado

---
+++ src/container/DetalleDepartment.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DetalleDepartment = () => {
+  return (
+    <article className="block p-6 bg-white rounded-lg border border-gray-200 sm:mt-10 mb-8">
+
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      Departamento 1
+    </h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400 text-justif max-w-xl">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam iure voluptatum vel nesciunt magni minus accusamus aut dolor aspernatur, magnam odio libero vitae minima, consequatur impedit vero omnis placeat ut!
+    </p>
+
+    <h5 className="font-bold text-lg mt-6">Ciudades</h5>
+  </article> 
+  )
+}
+
+export default DetalleDepartment
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Table from '../components/Table'
+import DetalleDepartment from './DetalleDepartment'
 
 const Home = () => {
   return (
     <>
         <Navbar/>
         <Table/>
+        <DetalleDepartment/>
     </>
   )
 }
